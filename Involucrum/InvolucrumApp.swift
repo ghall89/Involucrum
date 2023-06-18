62dbc1f490dfd80e911c1476b1791b2b29629c55
Graham Hall <graham@ghall.dev>
2023-06-18 13:55:41 -0400

    Initial Commit

---
+++ Involucrum/InvolucrumApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvolucrumApp.swift
+//  Involucrum
+//
+//  Created by Graham Hall on 6/18/23.
+//
+
+import SwiftUI
+
+@main
+struct InvolucrumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
